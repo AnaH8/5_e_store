3bb74aeccc33c1ca8717af4cce3a693c7be473b1
ana.herbel <anamariaherbel@gmail.com>
2025-03-21 19:15:55 +0200

    add main content

--- components/ImageBanner.jsx
+++ components/ImageBanner.jsx
@@ -1,5 +1,40 @@
+'use client'
+
+import { useState, useRef, useEffect } from "react"
+
 export default function ImageBanner() {
+
+  const [isLoaded, setIsLoaded] = useState(false)
+  const imgRef = useRef()
+
+  //if the image is completely loaded (nextJS chaces images)
+  useEffect(()=>{
+    if (imgRef.current.complete){
+      setIsLoaded(true)
+    }
+  },[])
+
   return (
-    <div></div>
+    <div className="banner-images">
+      <img className="low-res-img" src="low_res/banner.jpeg" alt="banner-low-res"/>
+      <img ref={imgRef} className="high-res-img" src="med_res/banner.png" alt="banner-high-res" style={{opacity: isLoaded ? 1 : 0}} 
+      onLoad={()=>{
+        //when the high res image is completely loaded, this callback will be executed 
+        //and the intention is to make this initially invisible image, visible
+        setIsLoaded(true)
+      }}/>
+      <div className="cta-btns-container">
+        <div>
+          <div>
+            <h3>Welcome to</h3>
+            <h1>E-Store</h1>
+          </div>
+          <div>
+            <button>Shop stickers</button>
+            <button>Shop planner</button>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
