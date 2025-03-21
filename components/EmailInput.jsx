5510e73925a2ed1f0ec61fa2150ed1a8454d13ba
ana.herbel <anamariaherbel@gmail.com>
2025-03-21 14:20:35 +0200

    style footer

---
+++ components/EmailInput.jsx
@@ -0,0 +1,10 @@
+'use client'
+
+export default function EmailInput() {
+  return (
+    <div className="sign-up">
+        <input placeholder="email address..."/>
+        <button className="button-card">Sign Up</button>
+    </div>
+  )
+}
