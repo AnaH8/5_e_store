bc33b41aec8af0af70f981cb0008744b7560f827
ana.herbel <anamariaherbel@gmail.com>
2025-03-24 13:58:15 +0200

    init stripe and fetch products

---
+++ app/api/products/route.js
@@ -0,0 +1,45 @@
+import Stripe from "stripe"
+import "../../../envConfig.js"
+
+const API_KEY = process.env.STRIPE_SECRET_KEY
+
+const stripe = new Stripe(API_KEY)
+
+export async function GET(){
+    try {
+        //fetch all the active products from stripe
+        const products = await stripe.products.list({ active: true })
+
+
+        //fetch all the active prices
+        const prices = await stripe.prices.list({ active: true })
+
+
+        //combine products and their associated prices
+        //price is a particular variant of a product
+        const combinedData = products.data.map((product)=>{
+            const productPrices = prices.data.filter((price)=>{      
+                return price.product === product.id                  //get all the prices relevant to a particular product
+            })
+
+            return {
+                ...product,
+                prices: productPrices.map((price)=>{            //only the data that we need 
+                    return {
+                        id: price.id,
+                        unit_amount: price.unit_amount,
+                        currency: price.currency,
+                        recurring: price.recurring
+                    }
+                })
+            }
+        })
+
+        //send combined data as json
+        return Response.json(combinedData)
+
+    } catch (err) {
+        console.error('Error fetching data from stripe: ', err.message)
+        return Response.json({error: 'Failed to fetch data from stripe'})
+    }
+}
